1887bfaba6972f17a04ebbbed8f68175676a8eda
Aarion Ojeda <aarion.ojeda01@gmail.com>
2022-09-20 15:25:33 -0500

    user boilerplate

---
+++ ratemyhike/src/main/java/com/example/ratemyhike/Exceptions/UserWithUsernameDoesntExist.java
@@ -0,0 +1,4 @@
+package com.example.ratemyhike.Exceptions;
+
+public class UserWithUsernameDoesntExist extends Exception {
+}
--- ratemyhike/src/main/java/com/example/ratemyhike/Service/UserService.java
+++ ratemyhike/src/main/java/com/example/ratemyhike/Service/UserService.java
@@ -1,4 +1,16 @@
 package com.example.ratemyhike.Service;
 
+import com.example.ratemyhike.Exceptions.UserWithIdDoesntExist;
+import com.example.ratemyhike.Exceptions.UserWithUsernameDoesntExist;
+import com.example.ratemyhike.Model.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<User> getAllUsers();
+
+    User getUserById(long id) throws UserWithIdDoesntExist;
+
+    User getUserByName(String name) throws UserWithUsernameDoesntExist;
 }
