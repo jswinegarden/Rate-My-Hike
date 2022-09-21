a0cdacb8e9a366736240630f1c914f41411523a8
Aarion Ojeda <aarion.ojeda01@gmail.com>
2022-09-21 11:35:21 -0500

    User service heavily expanded with create/update/delete, user controller updated to include login/register backend functionality

--- rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Model/User.java
+++ rate-My-Hike-BackEnd/src/main/java/com/example/ratemyhike/Model/User.java
@@ -1,19 +1,29 @@
 package com.example.ratemyhike.Model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
+    @Getter @Setter
     private long id;
 
+    @Getter @Setter
     private String username;
 
+    @Getter @Setter
     private String email;
 
+    @Getter @Setter
     private String password;
 
 
